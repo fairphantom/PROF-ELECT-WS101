acf9198dc4d43fdf0a12d1adfb59489549c0a124
fairphantom <fairphantom79@gmail.com>
2019-07-26 14:22:37 +0800

    buld

---
+++ img/script.js
@@ -0,0 +1,11 @@
+<script>
+function light(sw) {
+  var pic;
+  if (sw == 0) {
+    pic = "pic_bulboff.gif"
+  } else {
+    pic = "pic_bulbon.gif"
+  }
+  document.getElementById('myImage').src = pic;
+}
+</script>
